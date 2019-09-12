7f373eae848d25a2b2867fc70d7b95be067df2a7
Y45HK4R4ND1K4R <49726028+nerdstep710@users.noreply.github.com>
2019-09-12 16:31:13 -0500

    Update Gamepad.java

--- Gamepad.java
+++ Gamepad.java
@@ -1,1 +1,45 @@
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+@TeleOp(name="Gamepad", group="Linear OpMode")
+
+public class Gamepad extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    ColorSensor csensor;
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+    
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        leftMotor = hardwareMap.dcMotor.get("m3");
+        rightMotor = hardwareMap.dcMotor.get("m2");
+        csensor = hardwareMap.colorSensor.get("color");
+        csensor.enableLed(false);
+        
+        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        
+        
+        waitForStart();
+        runtime.reset();
+        
+        while (opModeIsActive()) {
+         /*Integer ci = csensor.argb();
+         String c = Integer.toString(ci);*/
+         
+         String c = "Red:"+csensor.red()+" Green:"+csensor.green()+" Blue:"+csensor.blue();
+         telemetry.addData("Status", "Color detected:" + c);
+         telemetry.update();
+         
+         leftMotor.setPower(-gamepad1.left_stick_y);
+         rightMotor.setPower(-gamepad1.right_stick_y);
+        }
+    }
+}
