67a00f09595528263d305a299c9ce4b0da3d0397
Y45HK4R4ND1K4R <49726028+nerdstep710@users.noreply.github.com>
2019-09-12 16:32:18 -0500

    Update API.java

--- API.java
+++ API.java
@@ -2,34 +2,56 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 public class API {
-  static void init(HardwareMap map) {
-    for (Motor m : Motor.values()) {
-      m.init(map);
+    
+    static void init(HardwareMap map) {
+        for (Motor m : Motor.values()) {
+            m.init(map);
+        }
+        Sensors.Color.sensor = map.colorSensor.get("color");
     }
     
+    public static class Sensors {
+        public static class Color {
+            static ColorSensor sensor;
+            public static int getColor() {
+                return sensor.argb();
+            }
+            public static int red() {
+                return sensor.red();
+            }
+            public static int green() {
+                return sensor.green();
+            }
+            public static int blue() {
+                return sensor.blue();
+            }
+        }
+    }
     
     public static enum Motor {
-      M0("m0");
-      private final String name;
-      private DcMotor motor;
-      Motor(String name) {
-        this.name= name;
-      }
-      
-      void init(HardwareMap map) {
-        motor = map.get(DcMotor.class, name);
-        motor.setPower(0);
-      }
-      
-      public void start(double power) {
-        motor.setPower(power);
-      }
-      
-      public void stop() {
-        motor.setPower(0);
-      }
+        M0("m0"), M1("m1"), M2("m2"), M3("m3");
+        private final String name;
+        private DcMotor motor;
+        Motor(String name) {
+            this.name = name;
+        }
+        
+        void init(HardwareMap map) {
+            motor = map.get(DcMotor.class, name);
+            motor.setPower(0);
+        }
+        
+        public void start(double power) {
+            try{
+                motor.setPower(power);
+            } catch (Exception e) {}    
+        }
+        
+        public void stop() {
+            motor.setPower(0);
+        }
     }
-  }
 }
