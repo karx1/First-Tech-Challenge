1ba5bb3a451bd56e46ceada4b3bc4ec6f2733856
Y45HK4R4ND1K4R <49726028+nerdstep710@users.noreply.github.com>
2019-09-23 16:08:10 -0500

    Update GamepadV2.java

--- GamepadV2.java
+++ GamepadV2.java
@@ -1,1 +1,24 @@
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="GamepadV2", group="Linear OpMode")
+
+public class GamepadV2 extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        API.init(hardwareMap);
+        while (opModeIsActive()) {
+            float left = gamepad1.left_stick_y+gamepad1.left_stick_x;
+            float right = gamepad1.left_stick_y-gamepad1.left_stick_x;
+            telemetry.addLine("Left: " + left + ", Right: " + right);
+            telemetry.update();
+            API.Motor.M2.start(-left);
+            API.Motor.M3.start(right);   
+        }
+    }
+}
